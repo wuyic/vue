9457da9062effd95449b6083415074e0d91e6cc4
xinfeng <wuyichao0@gmail.com>
2018-10-11 20:12:49 +0800

    注释 shared/util.js

--- src/shared/util.js
+++ src/shared/util.js
@@ -1,6 +1,6 @@
 /* @flow */
 /**
- * 该文件主要是一些公共的小组件，
+ * 该文件主要是一些公共的小组件
  * @type {Object}
  */
 
@@ -39,18 +39,20 @@ export function isPrimitive (value: any): boolean %checks {
 }
 
 /**
+ * 检测value是否为object
  * Quick object check - this is primarily used to tell
  * Objects from primitive values when we know the value
  * is a JSON-compliant type.
- * 检测value是否为object
  */
 export function isObject (obj: mixed): boolean %checks {
   return obj !== null && typeof obj === 'object'
 }
 
 /**
- * Get the raw type string of a value e.g. [object Object]
  * 获取原始类型 比如 Function Object Array
+ * toString是Object的原型属性，call是吧value当为this并执行toString函数
+ * 例如 function print() {return this.length;} print.call('123') //输出结果为 3
+ * Get the raw type string of a value e.g. [object Object]
  */
 const _toString = Object.prototype.toString
 
@@ -87,6 +89,11 @@ export function isValidArrayIndex (val: any): boolean {
 }
 
 /**
+ * 将value转化为字符串
+ * JSON.stringify 有三个参数
+ *  第一个是需要转化的对象
+ *  第二个是 用于转换结果的函数或数组
+ *  第三个是 文本缩进 2代表缩进两个空格，也可以为字符串
  * Convert a value to a string that is actually rendered.
  */
 export function toString (val: any): string {
@@ -98,6 +105,7 @@ export function toString (val: any): string {
 }
 
 /**
+ * 将字符串转化为数字，如果转化失败，则返回源字符串
  * Convert a input value to a number for persistence.
  * If the conversion fails, return original string.
  */
@@ -107,6 +115,20 @@ export function toNumber (val: string): number | string {
 }
 
 /**
+ * 创建一个map并检测一个key是否在这个Map中 str是创建map的源字符串，通过','来分隔
+ * 这是flow语法看起来比较复杂，可以使用babel转码
+    function makeMap(str, expectsLowerCase) {
+	var map = Object.create(null);
+	var list = str.split(',');
+	for (var i = 0; i < list.length; i++) {
+		map[list[i]] = true;
+	}
+	return expectsLowerCase ? function (val) {
+		return map[val.toLowerCase()];
+	} : function (val) {
+		return map[val];
+	};
+}
  * Make a map and return a function for checking if a key
  * is in that map.
  */
@@ -125,16 +147,19 @@ export function makeMap (
 }
 
 /**
+ * 使用上面的makeMap方法制作 isBuiltInTag
  * Check if a tag is a built-in tag.
  */
 export const isBuiltInTag = makeMap('slot,component', true)
 
 /**
+ * 使用上面的makeMap方法制作 isReservedAttribute
  * Check if a attribute is a reserved attribute.
  */
 export const isReservedAttribute = makeMap('key,ref,slot,slot-scope,is')
 
 /**
+ * 从数组中移除一个元素，若有重复的元素只能移除第一个
  * Remove an item from an array
  */
 export function remove (arr: Array<any>, item: any): Array<any> | void {
@@ -147,6 +172,8 @@ export function remove (arr: Array<any>, item: any): Array<any> | void {
 }
 
 /**
+ * 检测一个对象是否拥有某一个自身的属性 hasOwnProperty只会在对象本身查找
+ *
  * Check whether the object has the property.
  */
 const hasOwnProperty = Object.prototype.hasOwnProperty
@@ -155,6 +182,14 @@ export function hasOwn (obj: Object | Array<*>, key: string): boolean {
 }
 
 /**
+ * 自定一个缓存版本 如果有这个缓存版本就取出，否则插入
+ * function cached(fn) {
+      var cache = Object.create(null);
+      return function cachedFn(str) {
+        var hit = cache[str];
+       return hit || (cache[str] = fn(str));
+  };
+}
  * Create a cached version of a pure function.
  */
 export function cached<F: Function> (fn: F): F {
@@ -166,6 +201,7 @@ export function cached<F: Function> (fn: F): F {
 }
 
 /**
+ * 匹配一个单字字符（字母、数字或者下划线）。 并将其转化为大写字母， 然后缓存其值
  * Camelize a hyphen-delimited string.
  */
 const camelizeRE = /-(\w)/g
@@ -174,6 +210,7 @@ export const camelize = cached((str: string): string => {
 })
 
 /**
+ * 将字符串的首字母转化为大写
  * Capitalize a string.
  */
 export const capitalize = cached((str: string): string => {
@@ -181,6 +218,7 @@ export const capitalize = cached((str: string): string => {
 })
 
 /**
+ * 将单词中的字母转化为小写 \B 匹配一个单词的边界 = \B的前后 都是“字”字符或者都不是“字”字符
  * Hyphenate a camelCase string.
  */
 const hyphenateRE = /\B([A-Z])/g
@@ -220,6 +258,7 @@ export const bind = Function.prototype.bind
   : polyfillBind
 
 /**
+ * 将对象数组转化为数组
  * Convert an Array-like object to a real Array.
  */
 export function toArray (list: any, start?: number): Array<any> {
@@ -233,6 +272,7 @@ export function toArray (list: any, start?: number): Array<any> {
 }
 
 /**
+ * 对象合并，原有则覆盖
  * Mix properties into target object.
  */
 export function extend (to: Object, _from: ?Object): Object {
@@ -243,6 +283,7 @@ export function extend (to: Object, _from: ?Object): Object {
 }
 
 /**
+ * 将包含对象的数组转化为对象
  * Merge an Array of Objects into a single Object.
  */
 export function toObject (arr: Array<any>): Object {
@@ -256,6 +297,7 @@ export function toObject (arr: Array<any>): Object {
 }
 
 /**
+ * 空函数
  * Perform no operation.
  * Stubbing args to make Flow happy without leaving useless transpiled code
  * with ...rest (https://flow.org/blog/2017/05/07/Strict-Function-Call-Arity/)
@@ -263,16 +305,21 @@ export function toObject (arr: Array<any>): Object {
 export function noop (a?: any, b?: any, c?: any) {}
 
 /**
+ * 总是返回false
  * Always return false.
  */
 export const no = (a?: any, b?: any, c?: any) => false
 
 /**
+ * 返回本身
  * Return same value
  */
 export const identity = (_: any) => _
 
 /**
+ * 获取 module中staticKeys组成的字符串
+ * reduce 方法接收一个函数作为累加器，数组中的每个值（从左到右）开始缩减，最终计算为一个值。
+ * reduce array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
  * Generate a static keys string from compiler modules.
  */
 export function genStaticKeys (modules: Array<ModuleOptions>): string {
@@ -282,6 +329,7 @@ export function genStaticKeys (modules: Array<ModuleOptions>): string {
 }
 
 /**
+ * 检测两个值是否在宽松相等
  * Check if two values are loosely equal - that is,
  * if they are plain objects, do they have the same shape?
  */
@@ -318,6 +366,9 @@ export function looseEqual (a: any, b: any): boolean {
   }
 }
 
+/**
+ * 返回val在数组中宽松相等的第一次出现下标
+ */
 export function looseIndexOf (arr: Array<mixed>, val: mixed): number {
   for (let i = 0; i < arr.length; i++) {
     if (looseEqual(arr[i], val)) return i
@@ -326,6 +377,7 @@ export function looseIndexOf (arr: Array<mixed>, val: mixed): number {
 }
 
 /**
+ * 确保某个函数只被调用一次
  * Ensure a function is called only once.
  */
 export function once (fn: Function): Function {
